bad7c20a4aeac5850dcd5bc8a9894ee3a95b5ae1
dokimion24 <leecy3610@gmail.com>
2025-07-09 22:32:49 +0900

    feat: integrate Auth, Users, Memos, and Statistics modules into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MemosModule } from './memos/memos.module';
+import { StatisticsService } from './statistics/statistics.service';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [PrismaModule, CategoriesModule],
+  imports: [PrismaModule, CategoriesModule, AuthModule, UsersModule, MemosModule, StatisticsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, StatisticsService],
 })
 export class AppModule {}
