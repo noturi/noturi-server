ba7d5dbba6f4e3c2487a9e7140771b39ab1521ea
dokimion24 <leecy3610@gmail.com>
2025-07-29 10:59:56 +0900

    feat: ConfigModule 추가 및 LoggerMiddleware를 개발 환경에서만 적용하도록 AppModule 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,35 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { MemosModule } from './memos/memos.module';
-import { StatisticsService } from './statistics/statistics.service';
+import { PrismaModule } from '../prisma/prisma.module';
 import { StatisticsModule } from './statistics/statistics.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, CategoriesModule, AuthModule, UsersModule, MemosModule, StatisticsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    CategoriesModule,
+    AuthModule,
+    UsersModule,
+    MemosModule,
+    StatisticsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, StatisticsService],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // 개발 환경에서만 로거 미들웨어 적용
+    if (process.env.NODE_ENV !== 'production') {
+      consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
+  }
+}
