c3b3cb74ec3b4cd000be804268e3b8881d939ae5
dokimion24 <leecy3610@gmail.com>
2025-07-30 18:28:11 +0900

    fix: 평점 유효성 검사 범위를 0.0 ~ 5.0으로 수정하고, 기본 메시지를 업데이트

--- src/common/validators/validator.ts
+++ src/common/validators/validator.ts
@@ -3,11 +3,11 @@ import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validat
 @ValidatorConstraint({ name: 'isValidRating', async: false })
 export class IsValidRating implements ValidatorConstraintInterface {
   validate(rating: number) {
-    // 1.0, 1.5, 2.0, 2.5, ... 5.0만 허용
-    return rating >= 1.0 && rating <= 5.0 && (rating * 2) % 1 === 0;
+    // 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, ... 5.0만 허용
+    return rating >= 0.0 && rating <= 5.0 && (rating * 2) % 1 === 0;
   }
 
   defaultMessage() {
-    return '별점은 0.5 단위로만 입력 가능합니다 (예: 1.0, 1.5, 2.0, ...)';
+    return '별점은 0.5 단위로만 입력 가능합니다 (0 ~ 5.0)';
   }
 }
