161a63f69290d51aaf539643863bdae1a7a5de34
dokimion24 <leecy3610@gmail.com>
2025-08-06 20:36:53 +0900

    feat: AuthenticatedRequest 인터페이스 추가 및 Express Request와의 통합

--- src/common/types/auth.types.ts
+++ src/common/types/auth.types.ts
@@ -1,4 +1,5 @@
 import { Category, User } from '@prisma/client';
+import { Request } from 'express';
 
 export interface UserWithCategories extends User {
   categories: Category[];
@@ -6,4 +7,8 @@ export interface UserWithCategories extends User {
 
 export interface AuthenticatedUser extends Omit<User, 'createdAt' | 'updatedAt' | 'provider' | 'providerId'> {
   categories: Category[];
+}
+
+export interface AuthenticatedRequest extends Request {
+  user: AuthenticatedUser;
 }
