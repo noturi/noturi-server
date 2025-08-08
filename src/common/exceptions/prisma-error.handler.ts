b4eec65c4d3b1b176e008d18cc1f4fa9f14ca765
dokimion24 <leecy3610@gmail.com>
2025-08-08 17:30:06 +0900

    feat: Prisma 오류 처리 방식을 개선하여 HttpException으로 변경하고, 애플리케이션 전용 에러 코드 및 상세 메시지를 포함하도록 수정

--- src/common/exceptions/prisma-error.handler.ts
+++ src/common/exceptions/prisma-error.handler.ts
@@ -1,25 +1,65 @@
-import { ConflictException, NotFoundException, BadRequestException } from '@nestjs/common';
+import { ConflictException, NotFoundException, BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { AppErrorCode } from '../errors/app-error-codes';
 
 export class PrismaErrorHandler {
   static handle(error: unknown): never {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       switch (error.code) {
-        case 'P2002':
-          throw new ConflictException('이미 존재하는 데이터입니다');
+        case 'P2002': {
+          throw new HttpException(
+            {
+              statusCode: HttpStatus.CONFLICT,
+              code: AppErrorCode.DUPLICATE_RESOURCE,
+              message: '이미 존재하는 데이터입니다',
+            },
+            HttpStatus.CONFLICT,
+          );
+        }
         case 'P2025':
-          throw new NotFoundException('데이터를 찾을 수 없습니다');
+          throw new HttpException(
+            {
+              statusCode: HttpStatus.NOT_FOUND,
+              code: AppErrorCode.CATEGORY_NOT_FOUND,
+              message: '데이터를 찾을 수 없습니다',
+            },
+            HttpStatus.NOT_FOUND,
+          );
         case 'P2003':
-          throw new BadRequestException('참조 무결성 제약 조건 위반입니다');
+          throw new HttpException(
+            {
+              statusCode: HttpStatus.BAD_REQUEST,
+              code: AppErrorCode.FK_CONSTRAINT_VIOLATION,
+              message: '참조 무결성 제약 조건 위반입니다',
+            },
+            HttpStatus.BAD_REQUEST,
+          );
         case 'P2014':
-          throw new BadRequestException('유효하지 않은 ID입니다');
+          throw new HttpException(
+            { statusCode: HttpStatus.BAD_REQUEST, code: AppErrorCode.INVALID_ID, message: '유효하지 않은 ID입니다' },
+            HttpStatus.BAD_REQUEST,
+          );
         default:
-          throw new BadRequestException('데이터베이스 오류가 발생했습니다');
+          throw new HttpException(
+            {
+              statusCode: HttpStatus.BAD_REQUEST,
+              code: AppErrorCode.VALIDATION_ERROR,
+              message: '데이터베이스 오류가 발생했습니다',
+            },
+            HttpStatus.BAD_REQUEST,
+          );
       }
     }
-    
+
     if (error instanceof Prisma.PrismaClientValidationError) {
-      throw new BadRequestException('잘못된 데이터 형식입니다');
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          code: AppErrorCode.VALIDATION_ERROR,
+          message: '잘못된 데이터 형식입니다',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
     throw error;
