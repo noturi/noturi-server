219d63ee34bc063dace92971c130550cc180750d
dokimion24 <leecy3610@gmail.com>
2025-09-08 16:27:25 +0900

    feat: ErrorResponseDto의 HTTP 상태 코드 및 메시지 수정

--- src/common/dto/error-response.dto.ts
+++ src/common/dto/error-response.dto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ErrorResponseDto {
-  @ApiProperty({ example: 409, description: 'HTTP 상태 코드' })
+  @ApiProperty({ example: 404, description: 'HTTP 상태 코드' })
   statusCode: number;
 
-  @ApiProperty({ example: 4092, description: '애플리케이션 상세 에러 코드(4자리)', required: false })
+  @ApiProperty({ example: 4041, description: '애플리케이션 상세 에러 코드(4자리)', required: false })
   code?: number;
 
-  @ApiProperty({ example: '메모가 있는 카테고리는 삭제할 수 없습니다', description: '에러 메시지' })
+  @ApiProperty({ example: '리소스를 찾을 수 없습니다', description: '에러 메시지' })
   message: string;
 
-  @ApiProperty({ example: { memoCount: 3 }, description: '추가 상세 정보', required: false })
+  @ApiProperty({ example: {}, description: '추가 상세 정보', required: false })
   details?: unknown;
 }
